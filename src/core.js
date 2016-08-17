613ea083b83e0758db9dd9964d5862a7c8439686
leesungju <lsj4401@naver.com>
2016-08-17 22:11:33 +0900

    init sample

---
+++ src/core.js
@@ -0,0 +1,20 @@
+//noinspection JSAnnotator
+/**
+ * Created by lsj on 2016-08-17.
+ */
+'use strict'
+
+(function (window) {
+    window.vcjs = {
+        data: {},
+        draw: function() {
+
+        },
+        init: function(data) {
+            this.data = data;
+        }
+    }
+
+    vcjs.init();
+    vcjs.draw();
+})(window);
